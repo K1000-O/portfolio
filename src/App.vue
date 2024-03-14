fcae815072aabdba6764ff12ccf8d83e94b00f4b
CAMILO JENE CONDE <camilo.jene.ext@inetum.com>
2024-03-14 13:39:09 +0100

    Actualizar estilos y agregar componente de trabajo

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <script setup>
   import Header from './components/Header.vue'
   import AboutMe from './components/AboutMe.vue'
+  import Work from './components/Work.vue';
 </script>
 
 <template>
   <Header></Header>
 
-  <div class="sm:pt-16 lg:mx-14">
+  <div class="sm:pt-16 lg:mx-14 flex flex-col gap-20">
     <AboutMe></AboutMe>
+    <Work></Work>
   </div>
 
 </template>
--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -1,22 +1,24 @@
 <template>
-	<div class="title flex items-center gap-2">
-		<img class="w-8 sm:w-10 lg:w-12" src="/sobreMi-icon.svg" alt="logo">
-		<h1 class="text-4xl sm:text-5xl font-semibold font-noto-serif text-[--tiffany-blue] uppercase">Sobre mi ( ) {</h1>
-	</div>
-	<div class="body pt-5 xl:pt-10 flex flex-col items-center xl:flex-row justify-center gap-2">
-		<div class="texto order-2 xl:order-1 max-md:">
-			<p class="text-sm lg:text-xl font-semibold font-poppins text-[--light-cyan] uppercase">¡Hola! Me llamo <span class="text-[--tiffany-blue] "> Camilo Jené </span> y soy ingeniero informático. Desde siempre
-			me han gustado los ordenadores debido a la complejidad de ellos y lo que nos aportan en la vida.</p>
-			<br>
-			<p class="text-sm lg:text-xl font-semibold font-poppins text-[--light-cyan] uppercase">Soy un apasionado de la tecnología y del desarrollo de software. Me encanta aprender cosas nuevas y
-			ver como lo que tenía en mi cabeza poco a poco va cogiendo vida.</p>
-
-			<br>
-			<p class="text-sm lg:text-xl font-semibold font-poppins text-[--light-cyan] uppercase">Me considero una persona <span class="text-[--tiffany-blue] "> responsable </span>, con una <span class="text-[--tiffany-blue] "> gran motivación </span> y un <span class="text-[--tiffany-blue] "> gran espíritu de superación </span>.
-			Desde 2018, me dedico además a uno de mis grandes hoobies: entrenar en diferentes categorias del fútbol base.</p>
+	<div class="card">
+		<div class="title flex items-center gap-2">
+			<img class="w-8 sm:w-10 lg:w-12" src="/sobreMi-icon.svg" alt="logo">
+			<h1 class="text-4xl sm:text-5xl font-semibold font-noto-serif text-white uppercase">Sobre mi ( ) {</h1>
 		</div>
+		<div class="body pt-5 xl:pt-10 flex flex-col items-center xl:flex-row justify-center gap-2">
+			<div class="texto order-2 xl:order-1">
+				<p class="text-sm lg:text-base font-semibold font-poppins text-white uppercase">¡Hola! Me llamo <span class="text-[--tiffany-blue] "> Camilo Jené </span> y soy ingeniero informático. Desde siempre
+				me han gustado los ordenadores debido a la complejidad de ellos y lo que nos aportan en la vida.</p>
+				<br>
+				<p class="text-sm lg:text-base font-semibold font-poppins text-white uppercase">Soy un apasionado de la tecnología y del desarrollo de software. Me encanta aprender cosas nuevas y
+				ver como lo que tenía en mi cabeza poco a poco va cogiendo vida.</p>
 
-		<img class="w-52 md:w-60 lg:w-72 xl:ml-14 order-1" src="/foto-perfil.svg" alt="logo">
+				<br>
+				<p class="text-sm lg:text-base font-semibold font-poppins text-white uppercase">Me considero una persona <span class="text-[--tiffany-blue] "> responsable </span>, con una <span class="text-[--tiffany-blue] "> gran motivación </span> y un <span class="text-[--tiffany-blue] "> gran espíritu de superación </span>.
+				Desde 2018, me dedico además a uno de mis grandes hoobies: entrenar en diferentes categorias del fútbol base.</p>
+			</div>
 
+			<img class="w-52 md:w-60 lg:w-72 xl:ml-14 order-1" src="/foto-perfil.svg" alt="logo">
+
+		</div>
 	</div>
 </template>
