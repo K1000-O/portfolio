a50898a4090cfd0996af2984103d9d1663e8c8f0
CAMILO JENE CONDE <camilo.jene.ext@inetum.com>
2024-03-15 15:39:47 +0100

    Actualizar componente Work.vue con información de empresa

--- src/components/Work.vue
+++ src/components/Work.vue
@@ -7,14 +7,16 @@
     <div class="body pt-5 xl:pt-10 flex flex-col items-center xl:flex-row justify-end gap-2">
       <ol class="relative border-s-2 md:border-s-0 md:border-e-4 border-gray-900 dark:border-[--light-cyan]">
           <li class="mb-10 ms-6">
-              <h3 class="flex md:justify-end items-center mb-3 text-3xl font-semibold font-jetbrains text-[#0f5674] dark:text-[--tiffany-blue] uppercase">INETUM <span class="bg-blue-100 text-blue-800 text-lg font-medium me-4 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300 ms-3">Actual</span></h3>
+              <div class="absolute mt-3 md:mt-6 ml-[-31px] md:ml-0 md:mr-[-10px] md:right-0 w-3 h-3 md:w-4 md:h-4 bg-[#C2DE21] rounded-full border-2 border-[--light-cyan] dark:border-[--indigo-dye]"></div>
+              <h3 class="flex md:justify-end items-center mb-3 text-3xl font-semibold font-jetbrains text-[#0f5674] dark:text-[--tiffany-blue] uppercase"> <object data="/RVB_INETUM.svg" class="w-32 sm:w-40 lg:w-52 dark:hidden"></object> <object data="/R_INETUM.svg" class="w-32 sm:w-40 lg:w-52 hidden dark:block"></object> <span class="bg-blue-100 text-blue-800 text-lg font-medium me-4 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300 ms-3">Actual</span></h3>
               <time class="flex items-center md:justify-end md:px-4 mb-2 text-base font-normal font-biryani leading-none text-gray-900 dark:text-[--moonstone]">Desarrollador de software Jr</time>
               <p class="text-lg font-normal flex md:justify-end md:px-4 font-poppins text-gray-500 dark:text-white"> <span class="block md:hidden">-&nbsp; </span> Desarrollo de aplicaciones web con Spring Boot</p>
               <p class="text-lg font-normal flex md:justify-end md:px-4 font-poppins text-gray-500 dark:text-white"> <span class="block md:hidden">-&nbsp; </span> Gestión de bases de datos</p>
               <p class="mb-4 text-lg font-normal flex md:justify-end md:px-4 font-poppins text-gray-500 dark:text-white"> <span class="block md:hidden">-&nbsp; </span> Resolución de incidencias</p>
           </li>
           <li class="ms-6">
-              <h3 class="flex md:justify-end items-center mb-3 md:px-4 text-3xl font-semibold font-jetbrains text-[#0f5674] dark:text-[--tiffany-blue] uppercase">Inetum</h3>
+              <div class="absolute mt-3 md:mt-6 ml-[-31px] md:ml-0 md:mr-[-10px] md:right-0 w-3 h-3 md:w-4 md:h-4 bg-[#C2DE21] rounded-full border-2 border-[--light-cyan] dark:border-[--indigo-dye]"></div>
+              <h3 class="flex md:justify-end items-center mb-3 text-3xl font-semibold font-jetbrains text-[#0f5674] dark:text-[--tiffany-blue] uppercase"><object data="/RVB_INETUM.svg" class="w-32 sm:w-40 lg:w-52 dark:hidden"></object> <object data="/R_INETUM.svg" class="w-32 sm:w-40 lg:w-52 hidden dark:block"></object></h3>
               <time class="flex items-center md:justify-end md:px-4 mb-2 text-base font-normal font-biryani leading-none text-gray-900 dark:text-[--moonstone]">Prácticas de empresa</time>
           </li>
       </ol>
