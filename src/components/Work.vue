a1e3afc782249ebc6951fbff727c2efe89c281fe
K1000-O <cjeneconde@gmail.com>
2024-03-19 19:12:32 +0100

    Actualización de estilos en el componente Work.vue

--- src/components/Work.vue
+++ src/components/Work.vue
@@ -7,18 +7,18 @@
     
     <article class="pt-5 sm:pt-10 flex flex-col xl:flex-row justify-end gap-2">
       <ol class="relative border-s-2 sm:border-s-0 sm:border-e-4 border-gray-900 dark:border-[--light-cyan]">
+        <h3 class="flex md:justify-end items-center mb-3 text-3xl font-semibold font-jetbrains text-[#0f5674] dark:text-[--tiffany-blue] uppercase"> <img src="/RVB_INETUM.svg" class="w-32 sm:w-40 lg:w-52 dark:hidden"></img> <img src="/R_INETUM.svg" class="w-32 sm:w-40 lg:w-52 hidden dark:block"></img> <span class="bg-blue-100 text-blue-800 text-lg font-medium me-4 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300 ms-3">Actual</span></h3>
+
           <li class="mb-10 ms-6">
-              <div class="absolute mt-3 md:mt-6 ml-[-31px] md:ml-0 md:mr-[-10px] md:right-0 w-3 h-3 md:w-4 md:h-4 bg-[#C2DE21] rounded-full border-2 border-[--light-cyan] dark:border-[--indigo-dye]"></div>
-              <h3 class="flex md:justify-end items-center mb-3 text-3xl font-semibold font-jetbrains text-[#0f5674] dark:text-[--tiffany-blue] uppercase"> <img src="/RVB_INETUM.svg" class="w-32 sm:w-40 lg:w-52 dark:hidden"></img> <img src="/R_INETUM.svg" class="w-32 sm:w-40 lg:w-52 hidden dark:block"></img> <span class="bg-blue-100 text-blue-800 text-lg font-medium me-4 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300 ms-3">Actual</span></h3>
-              <time class="flex items-center md:justify-end md:px-4 mb-2 text-base font-normal font-biryani leading-none text-gray-900 dark:text-[--moonstone]">Desarrollador de software Jr</time>
+              <div class="absolute ml-[-31px] md:ml-0 md:mr-[-10px] md:right-0 w-3 h-3 md:w-4 md:h-4 bg-[#C2DE21] rounded-full border-2 border-[--light-cyan] dark:border-[--indigo-dye]"></div>
+              <p class="flex items-center md:justify-end md:px-4 mb-2 text-base font-normal font-biryani leading-none text-gray-900 dark:text-[--moonstone]">Desarrollador de software Jr</p>
               <p class="text-lg font-normal flex md:justify-end md:px-4 font-poppins text-gray-500 dark:text-white"> <span class="block md:hidden">-&nbsp; </span> Desarrollo de aplicaciones web con Spring Boot</p>
               <p class="text-lg font-normal flex md:justify-end md:px-4 font-poppins text-gray-500 dark:text-white"> <span class="block md:hidden">-&nbsp; </span> Gestión de bases de datos</p>
               <p class="mb-4 text-lg font-normal flex md:justify-end md:px-4 font-poppins text-gray-500 dark:text-white"> <span class="block md:hidden">-&nbsp; </span> Resolución de incidencias</p>
           </li>
           <li class="ms-6">
-              <div class="absolute mt-3 md:mt-6 ml-[-31px] md:ml-0 md:mr-[-10px] md:right-0 w-3 h-3 md:w-4 md:h-4 bg-[#C2DE21] rounded-full border-2 border-[--light-cyan] dark:border-[--indigo-dye]"></div>
-              <h3 class="flex md:justify-end items-center mb-3 text-3xl font-semibold font-jetbrains text-[#0f5674] dark:text-[--tiffany-blue] uppercase"><img src="/RVB_INETUM.svg" class="w-32 sm:w-40 lg:w-52 dark:hidden"></img> <img src="/R_INETUM.svg" class="w-32 sm:w-40 lg:w-52 hidden dark:block"></img></h3>
-              <time class="flex items-center md:justify-end md:px-4 mb-2 text-base font-normal font-biryani leading-none text-gray-900 dark:text-[--moonstone]">Prácticas de empresa</time>
+              <div class="absolute ml-[-31px] md:ml-0 md:mr-[-10px] md:right-0 w-3 h-3 md:w-4 md:h-4 bg-[#C2DE21] rounded-full border-2 border-[--light-cyan] dark:border-[--indigo-dye]"></div>
+              <p class="flex items-center md:justify-end md:px-4 mb-2 text-base font-normal font-biryani leading-none text-gray-900 dark:text-[--moonstone]">Prácticas de empresa</p>
           </li>
       </ol>
     </article>
