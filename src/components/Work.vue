41c25f31874fa30523f2ea6f356031074fca1f1f
CAMILO JENE CONDE <camilo.jeneconde.ext@telefonica.com>
2025-01-22 00:48:21 +0100

    Modificado alt del los logos de las empresas

--- src/components/Work.vue
+++ src/components/Work.vue
@@ -8,14 +8,14 @@
     <article class="pt-5 sm:pt-10 flex flex-col xl:flex-row justify-end gap-2">
 
       <ol class="relative border-s-2 sm:border-s-0 sm:border-e-4 border-gray-900 dark:border-[--light-cyan]">
-        <h3 class="flex md:justify-end items-center mb-3 text-3xl font-semibold font-jetbrains text-[#0f5674] dark:text-[--tiffany-blue] uppercase"> <img src="/capgemini.svg" class="w-32 sm:w-40 lg:w-52 dark:hidden"></img> <img src="/capgemini_dark.svg" class="w-32 sm:w-40 lg:w-52 hidden dark:block"></img> <span class="bg-blue-100 text-blue-800 text-lg font-medium me-4 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300 ms-3">Actual</span></h3>
+        <h3 class="flex md:justify-end items-center mb-3 text-3xl font-semibold font-jetbrains text-[#0f5674] dark:text-[--tiffany-blue] uppercase"> <img src="/capgemini.svg" class="w-32 sm:w-40 lg:w-52 dark:hidden" alt="imagen capgemini"></img> <img src="/capgemini_dark.svg" class="w-32 sm:w-40 lg:w-52 hidden dark:block" alt="imagen capgemini oscuro"></img> <span class="bg-blue-100 text-blue-800 text-lg font-medium me-4 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300 ms-3">Actual</span></h3>
 
             <li class="mb-10 ms-6">
                 <div class="absolute ml-[-31px] md:ml-0 md:mr-[-10px] md:right-0 w-3 h-3 md:w-4 md:h-4 bg-[#C2DE21] rounded-full border-2 border-[--light-cyan] dark:border-[--indigo-dye]"></div>
                 <p class="flex items-center md:justify-end md:px-4 mb-2 text-base font-normal font-biryani leading-none text-gray-900 dark:text-[--moonstone]">Desarrollador de software Jr</p>
             </li>
 
-        <h3 class="flex md:justify-end items-center mb-3 text-3xl font-semibold font-jetbrains text-[#0f5674] dark:text-[--tiffany-blue] uppercase"> <img src="/RVB_INETUM.svg" class="w-32 sm:w-40 lg:w-52 dark:hidden"></img> <img src="/R_INETUM.svg" class="w-32 sm:w-40 lg:w-52 hidden dark:block"></img>
+        <h3 class="flex md:justify-end items-center mb-3 text-3xl font-semibold font-jetbrains text-[#0f5674] dark:text-[--tiffany-blue] uppercase"> <img src="/RVB_INETUM.svg" class="w-32 sm:w-40 lg:w-52 dark:hidden" alt="imagen inetum"></img> <img src="/R_INETUM.svg" class="w-32 sm:w-40 lg:w-52 hidden dark:block" alt="imagen inetum oscuro"></img>
           <span class="text-blue-800 text-xs me-4 dark:text-blue-300 ms-3">jul 2023 - dic 2024</span>
         </h3>
 
